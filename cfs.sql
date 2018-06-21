92d985a1562bd07cf024dd069cededacfa8992a7
tawhidkhn63 <39498885+tawhidkhn63@users.noreply.github.com>
2018-06-21 14:43:46 -0400

    Add files via upload

--- cfs.sql
+++ cfs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 18, 2018 at 08:28 PM
+-- Generation Time: Jun 21, 2018 at 08:39 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.2.5
 
@@ -77,6 +77,32 @@ INSERT INTO `rooms` (`ROOM_ID`, `BUILDING`, `ROOM_NUM`, `CAP`) VALUES
 (19, ' Mueller Center', ' Basketball Court', 20),
 (20, ' Rensselaer Union', ' Games Room', 40);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tomorrow_reservations`
+--
+
+CREATE TABLE `tomorrow_reservations` (
+  `RESERVATION_ID` int(10) NOT NULL,
+  `START_TIME` datetime NOT NULL,
+  `END_TIME` datetime NOT NULL,
+  `DESCRIPTION` varchar(50) NOT NULL,
+  `APPROVED` tinyint(1) NOT NULL,
+  `ROOM_ID` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tomorrow_reservations`
+--
+
+INSERT INTO `tomorrow_reservations` (`RESERVATION_ID`, `START_TIME`, `END_TIME`, `DESCRIPTION`, `APPROVED`, `ROOM_ID`) VALUES
+(1, '2018-06-18 12:00:00', '2018-06-18 13:00:00', 'Fortnite Tournament', 1, 16),
+(2, '2018-06-18 15:00:00', '2018-06-18 22:00:00', 'Badmington Club meeting', 1, 20),
+(3, '2018-06-18 00:00:00', '2018-06-19 00:00:00', 'Nothing lol', 0, 13),
+(4, '2018-06-18 20:00:00', '2018-06-18 22:00:00', 'Laser Tag', 1, 13),
+(5, '2018-06-18 17:00:00', '2018-06-18 19:00:00', 'Paintball fight', 1, 19);
+
 --
 -- Indexes for dumped tables
 --
@@ -94,6 +120,14 @@ ALTER TABLE `reservations`
 --
 ALTER TABLE `rooms`
   ADD PRIMARY KEY (`ROOM_ID`);
+
+--
+-- Indexes for table `tomorrow_reservations`
+--
+ALTER TABLE `tomorrow_reservations`
+  ADD PRIMARY KEY (`RESERVATION_ID`),
+  ADD KEY `ROOM_ID` (`ROOM_ID`),
+  ADD KEY `ROOM_ID_2` (`ROOM_ID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
