70d421f26474208198eac9928182263cc5d9a5f9
sjhuang26 <sjhuang26@gmail.com>
2018-07-12 15:48:54 -0400

    Initial work on schedule_main page

--- vue-template/.eslintrc.js
+++ vue-template/.eslintrc.js
@@ -50,6 +50,8 @@ module.exports = {
       optionalDependencies: ['test/unit/index.js']
     }],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prefer-template': 'off',
+    'quote-props': 'off'
   }
 }
---
+++ vue-template/src/containers/schedule_main/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <date-picker :date.sync="date" />
+    <schedule :data="scheduleData" />
+  </div>
+</template>
+
+<script>
+import schedule from '../../components/schedule';
+import datePicker from '../../components/date-picker';
+
+const ONE_DAY = 1000 * 3600 * 24;
+const ONE_HOUR = 1000 * 3600;
+export default {
+  name: 'schedule-main',
+  components: {
+    schedule,
+    datePicker,
+  },
+  props: {
+    scheduleRows: Array,
+  },
+  data() {
+    return {
+      date: new Date(),
+    };
+  },
+  computed: {
+    scheduleData() {
+      return {
+        startTime: this.date,
+        endTime: new Date(this.date.getTime() + ONE_DAY),
+        timeIncrement: ONE_HOUR,
+        rows: this.scheduleRows,
+      };
+    },
+  },
+};
+</script>
+
