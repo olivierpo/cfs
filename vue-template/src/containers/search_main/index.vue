1088fc845f50cf8eb9f7aa58b159af26fe118a56
yungkyung <tchang422@gmail.com>
2018-07-15 23:54:12 -0400

    Added search component with auto complete

---
+++ vue-template/src/containers/search_main/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <!-- This needs to change -->
+    <autocomplete :rooms="['DCC', 'Amos Eaton', 'West Hall', 'Union', ]" ></autocomplete>
+  </div>
+</template>
+
+<script>
+  import AutoComplete from './components/AutoComplete';
+
+  export default {
+    name: 'app',
+    components: {
+      autocomplete: AutoComplete,
+    },
+  };
+
+</script>
