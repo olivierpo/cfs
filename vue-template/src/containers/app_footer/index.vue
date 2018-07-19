0015db7622646b0b5b6cc4e3781e8daa7752c5cc
Olivier Poulin <poulio445@gmail.com>
2018-07-19 13:41:05 -0400

    Initial header and footer without much style

---
+++ vue-template/src/containers/app_footer/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="footer d-flex" role="contentinfo">
+  <div class="position-relative d-flex flex-justify-between pt-6 pb-2 mt-6 f6 text-gray border-top border-gray-light ">
+    <ul class="list-style-none d-flex flex-wrap ">
+      <li class="mr-3">&copy; 2018 <span title="0.42237s from unicorn-66984c65d-4c5rr">GitHub</span>, Inc.</li>
+        <li class="mr-3"><a data-ga-click="Footer, go to terms, text:terms" href="https://github.com/site/terms">Terms</a></li>
+        <li class="mr-3"><a data-ga-click="Footer, go to privacy, text:privacy" href="https://github.com/site/privacy">Privacy</a></li>
+        <li class="mr-3"><a href="https://help.github.com/articles/github-security/" data-ga-click="Footer, go to security, text:security">Security</a></li>
+        <li class="mr-3"><a href="https://status.github.com/" data-ga-click="Footer, go to status, text:status">Status</a></li>
+        <li><a data-ga-click="Footer, go to help, text:help" href="https://help.github.com">Help</a></li>
+    </ul>
+
+    <a aria-label="Homepage" title="GitHub" class="footer-octicon" href="https://github.com">
+</a>
+   <ul class="list-style-none d-flex flex-wrap ">
+        <li class="mr-3"><a data-ga-click="Footer, go to contact, text:contact" href="https://github.com/contact">Contact GitHub</a></li>
+      <li class="mr-3"><a href="https://developer.github.com" data-ga-click="Footer, go to api, text:api">API</a></li>
+      <li class="mr-3"><a href="https://training.github.com" data-ga-click="Footer, go to training, text:training">Training</a></li>
+      <li class="mr-3"><a href="https://shop.github.com" data-ga-click="Footer, go to shop, text:shop">Shop</a></li>
+        <li class="mr-3"><a href="https://blog.github.com" data-ga-click="Footer, go to blog, text:blog">Blog</a></li>
+        <li><a data-ga-click="Footer, go to about, text:about" href="https://github.com/about">About</a></li>
+
+    </ul>
+  </div>
+  <div class="d-flex flex-justify-center pb-6">
+    <span class="f6 text-gray-light"></span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style>
+.footer{
+    height: 108px;
+  background-color: #ef3e29;
+  background-image: linear-gradient(to right,#ef2929,#f05429);
+  font-size: 12px;
+  padding-top: 40px;
+  padding-bottom: 8px;
+  margin-left: auto;
+  margin-right: auto;
+
+}
+.d-flex{
+    display:flex;
+}
+</style>
---
+++ vue-template/src/containers/app_navbar/index.vue
@@ -0,0 +1,37 @@
+<template>
+<!-- <b-navbar toggleable="md" type="dark" variant="dark" fixed="top"> -->
+  <v-ons-toolbar class="navbar">
+      <div class="left">
+        <v-ons-toolbar-button @click="$store.commit('splitter/toggle')">
+          <v-ons-icon class="menu-icon" icon="ion-navicon, material:md-menu"></v-ons-icon>
+        </v-ons-toolbar-button>
+      </div>
+    
+    <div class="center">WOOOOOOO!</div>
+    <div class="right">
+    <v-ons-toolbar-button @click="$store.commit('splitter/toggle')">
+          <v-ons-icon class="menu-icon" icon="ion-navicon, material:md-menu"></v-ons-icon>
+        </v-ons-toolbar-button></div>
+      </v-ons-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style>
+.center{
+  color: #fff;
+}
+.navbar{
+  height: 48px;
+  background-color: #ef3e29;
+  background-image: linear-gradient(to right,#ef2929,#f05429);
+}
+.menu-icon{
+  color:white;
+}
+
+</style>
